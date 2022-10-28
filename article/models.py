3c9b89dfcc207b9756fb0e390d5c7e4b86a40101
didjsrne <hyunjae0723@gmail.com>
2022-10-28 16:48:39 +0900

    models.py | author > user 로 이름 변경

--- article/models.py
+++ article/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User 
 # Create your models here.
 class Article(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
